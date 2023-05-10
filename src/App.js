31ff7f8b0a8b03436ec37db7c544dc97f2e7f143
Meenakshiratnawat <90496119+Meenakshiratnawat@users.noreply.github.com>
2023-05-10 06:54:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { useState,useEffect } from "react";
+import "./styles.css";
+import Data from "./Data.js";
+import { rotateFun } from "./rotation";
+
+function App() {
+  const subjects = ["Maths", "Physics", "Chemistry", "Biology"];
+  const [selectedSubject, setSelectedSubject] = useState("");
+  const [leftTableData, setLeftTableData] = useState([]);
+  const [rightTableData, setRightTableData] = useState([]);
+  const [rotationCount, setRotationCount] = useState(0);
+  
+  
+  const generateQuestionsArray = (subject) => {
+    const questions = Array(10)
+      .fill("")
+      .map(
+        (question, index) => `${index + 1}. ${subject} Question ${index + 1}`
+      );
+    return questions;
+  };
+useEffect(() => {
+    setRightTableData([]); // Clear right table data when subject changes
+  }, [selectedSubject]);
+  
+  const handleDropdownChange = (event) => {
+    const selectedSubject = event.target.value;
+    setSelectedSubject(selectedSubject);
+    const questions = generateQuestionsArray(selectedSubject);
+    const data = new Data(selectedSubject, questions);
+    setLeftTableData(data.questions);
+  };
+
+  const handleCopyButtonClick = () => {
+    setRightTableData([...leftTableData]);
+  };
+  
+  //randomize button click function
+  const handleRandomizeButtonClick = () => {
+    const randomizedQuestions = [...leftTableData].sort(
+      () => Math.random() - 0.5
+    );
+    setRightTableData(randomizedQuestions);
+  };
+  
+  //Rotate button click function
+  const handleRotateButtonClick = () => {
+    if (rightTableData.length === 0) {
+      const rotatedQuestions = rotateFun(
+        [...leftTableData],
+        leftTableData.length
+      );
+      setRightTableData(rotatedQuestions);
+    } else {
+      const rotatedQuestions = rotateFun(
+        [...rightTableData],
+        rightTableData.length
+      );
+      setRightTableData(rotatedQuestions);
+    }
+    setRotationCount(rotationCount + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">BISHOP HEBER SCHOOL</h1>
+      <div className="dropdown">
+        <select value={selectedSubject} onChange={handleDropdownChange}>
+          <option value="">Select a subject</option>
+          {subjects.map((subject, index) => (
+            <option key={index} value={subject}>
+              {subject}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="table-container">
+        <table className="question-table">
+          <thead>
+            <tr>
+              <th>Question Bank</th>
+            </tr>
+          </thead>
+          <tbody>
+            {leftTableData.map((question, index) => (
+              <tr key={index}>
+                <td>{question}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <table className="question-table">
+          <thead>
+            <tr>
+              <th>Question</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rightTableData.map((question, index) => (
+              <tr key={index}>
+                <td>{question}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="button-container">
+        <button onClick={handleCopyButtonClick} className="action-button">
+          Question Sequence
+        </button>
+        <button onClick={handleRotateButtonClick} className="action-button">
+          Rotate
+        </button>
+        <button onClick={handleRandomizeButtonClick} className="action-button">
+          Randomize
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
